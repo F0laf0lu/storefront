2fd2aaae4113421759d8b5fdfb65b22e486a3122
Folafolu <ofolafolu@gmail.com>
2023-01-07 17:03:18 +0100

    Removed playground.urls from urls file

--- storefront/urls.py
+++ storefront/urls.py
@@ -20,6 +20,5 @@ admin.site.site_header = 'Storefront Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
