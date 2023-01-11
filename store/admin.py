1eb46812e690184f102df6c8566d459eb7c6c06a
Folafolu <ofolafolu@gmail.com>
2023-01-11 04:56:57 +0100

    customizing admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from . models import Collection, Product, Customer
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'inventory_status']
+    list_per_page = 10
+    list_editable = ['price']
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_per_page = 10
+    list_editable = ['membership']
+
+
+admin.site.register(Collection)
+
+
--- store/models.py
+++ store/models.py
@@ -9,6 +9,13 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
 class Product(models.Model):
     title=models.CharField(max_length = 255)
     slug = models.SlugField()
@@ -18,6 +25,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotion = models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_CHOICES = [
