78927ac0b6c97e28e8ffc42d96c028a9ef58d1c4
Folafolu <ofolafolu@gmail.com>
2023-01-14 09:45:57 +0100

    added product count column

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.db.models.aggregates import Count
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
 from . models import Collection, Product, Customer
 
 # Register your models here.
@@ -22,6 +26,23 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
 
 
-admin.site.register(Collection)
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product_count']
+
+    @admin.display(ordering='product_count')
+    def product_count(self, collection):
+        url = (
+            reverse("admin:store_product_changelist") 
+            + '?' 
+            + urlencode({
+            'collection__id' : str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, collection.product_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            product_count=Count('product')
+            )
 
 
