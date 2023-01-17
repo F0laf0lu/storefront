2f4c5c2030d5d6f8f8497fbe310c741e1c78422b
Folafolu <ofolafolu@gmail.com>
2023-01-17 18:19:16 +0100

    created products api

--- store/views.py
+++ store/views.py
@@ -2,21 +2,42 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True, context={'request':request})
-    return Response(serializer.data)
+    if request.method =='GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        serializer.validated_data
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
-    product = get_object_or_404(Product ,pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        try:
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 @api_view()
 def collection_detail(request, pk):
